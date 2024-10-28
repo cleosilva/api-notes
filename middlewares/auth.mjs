a139fa457372b71ae1911fa50f4a8d9ab8b87378
Cleo Silva <cleo_silva1906@hotmail.com>
2024-10-28 08:12:31 -0300

    Implemented token validation for routes

--- middlewares/auth.mjs
+++ middlewares/auth.mjs
@@ -12,3 +12,18 @@ export const verifyToken = (req, res, next) => {
         res.status(400).json({ message: "Invalid token" });
     }
 };
+
+export const authenticateJWT = (req, res, next) => {
+    const token = req.header('Authorization')?.split(" ")[1]; // token should be a bearer <token>
+    if (!token) {
+        return res.sendStatus(403); // Forbiden
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) {
+            return res.sendStatus(403);
+        }
+        req.user = user;
+        next();
+    })
+}
