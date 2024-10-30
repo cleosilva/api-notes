1c881caec09240a82fa97b9c04ccc55820eea7d6
Cleo Silva <cleo_silva1906@hotmail.com>
2024-10-30 11:59:29 -0300

    Implemented test for methods: list, update and delete.

--- tests/task.test.js
+++ tests/task.test.js
@@ -1,13 +1,14 @@
 import request from "supertest";
 import app from "../app.mjs";
 
+const token = process.env.BEARER_TOKEN
+let taskId;
 
-describe('POST /api/tasks', () => {
+describe('Task API', () => {
     it('should create a new task', async () => {
-        const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3MWU5NGRlNDcxMWM1MmNmNGEyM2JjNyIsImlhdCI6MTczMDEyNDQ3NiwiZXhwIjoxNzMwMTI4MDc2fQ.HK76CwjViUJVWslJN2kNMdIpldw42WNAMmwGKwJvWbI";
         const taskData = {
-            title: "Study Java",
-            description: "Complete the Java API project",
+            title: "Study Node.js",
+            description: "Complete the Node.js API project",
             dueDate: "2024-11-02"
         };
 
@@ -19,5 +20,42 @@ describe('POST /api/tasks', () => {
         expect(response.statusCode).toBe(201);
         expect(response.body.title).toBe(taskData.title);
         expect(response.body.description).toBe(taskData.description);
+
+        taskId = response.body._id
     });
+
+    it("should get all the tasks for the user", async () => {
+        const response = await request(app)
+            .get('/api/tasks')
+            .set('Authorization', `Bearer ${token}`);
+
+        expect(response.statusCode).toBe(200);
+        expect(Array.isArray(response.body)).toBe(true);
+        expect(response.body.length).toBeGreaterThan(0);
+    });
+
+    it('should update a task', async () => {
+        const updatedData = {
+            title: "Study Node.js - Uptaded",
+            description: 'Complete the Node.js API project and update the docs',
+        };
+
+        const response = await request(app)
+            .put(`/api/tasks/${taskId}`)
+            .set('Authorization', `Bearer ${token}`)
+            .send(updatedData);
+
+        expect(response.statusCode).toBe(200);
+        expect(response.body.title).toBe(updatedData.title);
+        expect(response.body.description).toBe(updatedData.description);
+    });
+
+    it('should delete a task', async () => {
+        const response = await request(app)
+            .delete(`/api/tasks/${taskId}`)
+            .set('Authorization', `Bearer ${token}`);
+
+        expect(response.statusCode).toBe(200);
+    })
+
 });
