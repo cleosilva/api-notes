65f6e23b94a2fc7cc75e8a1afad7a2b22ceb187a
Cleo Silva <cleo_silva1906@hotmail.com>
2024-10-31 09:38:33 -0300

    Implemented logger for task

--- controllers/taskController.mjs
+++ controllers/taskController.mjs
@@ -1,10 +1,15 @@
 import { Task } from "../models/Task.mjs";
+import logger from '../utils/logger.mjs';
 
 export const createTask = async (req, res) => {
     try {
         const task = await Task.create({ ...req.body, userId: req.user.id });
+        logger.info(`Create task with ID: ${task._id} for user ${req.user.id}`);
+
         res.status(201).json(task);
+
     } catch (error) {
+        logger.error(`Error to create task: ${error.message}`);
         res.status(500).json({ message: error.message });
     }
 };
@@ -12,8 +17,10 @@ export const createTask = async (req, res) => {
 export const getTasks = async (req, res) => {
     try {
         const tasks = await Task.find({ userId: req.user.id });
+        logger.info(`Get tasks for user ${req.user.id}`);
         res.status(200).json(tasks);
     } catch (error) {
+        logger.error(`Error to get tasks: ${error.message}`);
         res.status(500).json({ message: error.message });
     }
 };
@@ -27,10 +34,13 @@ export const updateTask = async (req, res) => {
             { new: true, runValidators: true }
         );
         if (!task) {
+            logger.warn(`Task with ID ${id} not found for user ${req.user.id}`);
             return res.status(404).json({ message: "Task not found!" })
         }
+        logger.info(`Updated task with ID: ${id} for user ${req.user.id}`);
         res.status(200).json(task);
     } catch (error) {
+        logger.error(`Error to update task with ID ${id}: ${error.message}`);
         res.status(500).json({ message: error.message });
     }
 };
@@ -40,10 +50,13 @@ export const deleteTask = async (req, res) => {
     try {
         const task = await Task.findOneAndDelete({ _id: id, userId: req.user.id });
         if (!task) {
+            logger.warn(`Attempted delete on non-existent task with ID ${id} for user ${req.user.id}`);
             return res.status(404).json({ message: "Task not found!" });
         }
+        logger.info(`Deleted task with ID: ${id} for user ${req.user.id}`);
         res.status(200).json({ message: "Deleted task successfully!" })
     } catch (error) {
+        logger.error(`Error to delete task with ID ${id}: ${error.message}`);
         res.status(500).json({ message: error.message });
     }
 };
