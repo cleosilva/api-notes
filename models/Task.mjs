06f55d145d3e4fc285b96813c5bd8885b41ecbf2
Cleo Silva <cleo_silva1906@hotmail.com>
2024-10-31 13:24:04 -0300

    Updated Task Model for notes

--- models/Task.mjs
+++ models/Task.mjs
@@ -1,11 +1,19 @@
 import mongoose from "mongoose";
 
+const ckeckListItemSchema = new mongoose.Schema({
+    text: { type: String, required: true },
+    checked: { type: Boolean, default: false }
+})
+
 const taskSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    description: String,
-    status: { type: String, enum: ['pending', 'completed'], default: 'pending' },
-    dueData: { type: Date },
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
-}, { timestamps: true });
+    content: { type: String, required: false }, // Conteúdo da nota
+    labels: [{ type: String }], // Etiqueta (tags)
+    color: { type: String, default: '#ffffff' }, // Cor da nota, padrão branca
+    checklist: [ckeckListItemSchema],
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+});
 
 export const Task = mongoose.model('Task', taskSchema);
